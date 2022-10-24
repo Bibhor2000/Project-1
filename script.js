c1e3e9c719d78f2c4a811650725bd61fbc1c2b22
Bibhor2000 <daxter740@gmail.com>
2022-10-24 15:50:56 -0400

    javascript, finished prep for splitting a random word from an array

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+//subDisplay Assigning Object
+const subDisplay = {
+    display: document.querySelector('.sub-display'),
+}
+
 //subGuesses Assigning Object
 const subGuesses = {
     guessOne: document.querySelector('#one'),
@@ -7,19 +12,33 @@ const subGuesses = {
     guessFive: document.querySelector('#five'),
     guessSix: document.querySelector('#six'),
     guessSeven: document.querySelector('#seven'),
-    guessEight: document.querySelector('#eight')
+    guessEight: document.querySelector('#eight'),
+    letterDisplay: function() {
+
+    }
 }
 
 //subInput Assigning Object
 const subInput = {
+    storage: ['Order', 'Carry', 'Fight', 'Start', 'Drink'],
+    generator: document.querySelector('#generator'),
+    input: document.querySelector('.sub-input'),
     inputOne: document.querySelector('#first'),
     inputTwo: document.querySelector('#second'),
     inputThree: document.querySelector('#third'),
     inputFour: document.querySelector('#fourth'),
-    inputFive: document.querySelector('#fifth')
+    inputFive: document.querySelector('#fifth'),
+    inputDisplay: function() {
+        this.input = ''
+        word = this.storage[Math.floor(Math.random() * this.storage.length)]
+        splitWord = word.split('')
+        this.inputOne.innerText = splitWord[3]
+    }
 }
 
-//subDisplay Assigning Object
-const subDisplay = {
-    display: document.querySelector('.display')
-}
+//Event listeners
+inputButton = subInput.generator;
+inputButton.addEventListener('click', event => {
+    event.preventDefault()
+    subInput.inputDisplay()
+});
