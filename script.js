796f086062fe5320f86415df558b9916e30272fc
Bibhor2000 <daxter740@gmail.com>
2022-10-24 10:02:57 -0400

    starting on javascript variable assigning

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
-//Query Assigning
-const guesses = document.querySelector('.guesses');
-const display = document.querySelector('.display');
-const input = document.querySelector('.input');
-const subGuesses =  document.querySelector('.sub-guesses');
-const subDisplay = document.querySelector('.sub-display');
-const subInput = document.querySelector('.sub-input');
+//subGuesses Assigning Object
+const subGuesses = {
+    guessOne: document.querySelector('#one'),
+    guessTwo: document.querySelector('#two'),
+    guessThree: document.querySelector('#three'),
+    guessFour: document.querySelector('#four'),
+    guessFive: document.querySelector('#five'),
+    guessSix: document.querySelector('#six'),
+    guessSeven: document.querySelector('#seven'),
+    guessEight: document.querySelector('#eight')
+}
+
+//subInput Assigning Object
+const subInput = {
+    inputOne: document.querySelector('#first'),
+    inputTwo: document.querySelector('#second'),
+    inputThree: document.querySelector('#third'),
+    inputFour: document.querySelector('#fourth'),
+    inputFive: document.querySelector('#fifth')
+}
+
+//subDisplay Assigning Object
+const subDisplay = {
+    display: document.querySelector('.display')
+}
