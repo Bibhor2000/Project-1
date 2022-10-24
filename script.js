3f17c6953e2c2468c6a43b20010c32343449f582
Bibhor2000 <daxter740@gmail.com>
2022-10-24 09:41:31 -0400

    finished html css design with flexbox and assigned them in the js file

--- script.js
+++ script.js
@@ -2,3 +2,6 @@
 const guesses = document.querySelector('.guesses');
 const display = document.querySelector('.display');
 const input = document.querySelector('.input');
+const subGuesses =  document.querySelector('.sub-guesses');
+const subDisplay = document.querySelector('.sub-display');
+const subInput = document.querySelector('.sub-input');
