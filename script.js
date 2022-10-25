8e1ecea6777f783970359c7e0f86820cee3fa965
Bibhor2000 <daxter740@gmail.com>
2022-10-25 18:03:33 -0400

    incorrect letters display now

--- script.js
+++ script.js
@@ -17,35 +17,88 @@ const subGuesses = {
         let inputs = [`${subInput.inputOne.value}`, `${subInput.inputTwo.value}`, `${subInput.inputThree.value}`, `${subInput.inputFour.value}`, `${subInput.inputFive.value}`]
         let input = playerInputs.join('')
         let words = subInput.storage
+        let incorrectWords = [];
         promptDisplay = subDisplay.display.innerText
         promptOutput = subInput.prompts
+        console.log(promptDisplay)
+        console.log(promptOutput.prompt1)
+        this.guessOne.innerText = 'a'
         if (promptDisplay === promptOutput.prompt1) {
             let word = words[0]
+            console.log(input);
             if (word.substring(0, 1) === input.substring(0, 1) || word.substring(1, 2) === input.substring(1, 2) || word.substring(2, 3) === input.substring(2, 3) || word.substring(3, 4) === input.substring(3, 4) || word.substring(4, 5) === input.substring(4, 5)) {
 
-            } else if (word.substring(0, 1) !== input.substring(0, 1)) {
+            } if (word.substring(0, 1) !== input.substring(0, 1)) {
                 this.guessOne.innerText = input.substring(0, 1)
-            } else if (word.substring(1, 2) !== input.substring(1, 2)) {
+            } if (word.substring(1, 2) !== input.substring(1, 2)) {
                 this.guessTwo.innerText = input.substring(1, 2)
-            } else if (word.substring(2, 3) !== input.substring(2, 3)) {
+            } if (word.substring(2, 3) !== input.substring(2, 3)) {
                 this.guessThree.innerText = input.substring(2, 3)
-            } else if (word.substring(3, 4) !== input.substring(3, 4)) {
+            } if (word.substring(3, 4) !== input.substring(3, 4)) {
                 this.guessFour.innerText = input.substring(3, 4)
-            } else if (word.substring(4, 5) !== input.substring(4, 5)) {
+            } if (word.substring(4, 5) !== input.substring(4, 5)) {
                 this.guessFive.innerText = input.substring(4, 5)
             }
-        } else if (promptDisplay === promptOutput.prompt2) {
+        }  if (promptDisplay === promptOutput.prompt2) {
             let word = words[1]
-            
-        } else if (promptDisplay === promptOutput.prompt3) {
+            if (word.substring(0, 1) === input.substring(0, 1) || word.substring(1, 2) === input.substring(1, 2) || word.substring(2, 3) === input.substring(2, 3) || word.substring(3, 4) === input.substring(3, 4) || word.substring(4, 5) === input.substring(4, 5)) {
+
+            } if (word.substring(0, 1) !== input.substring(0, 1)) {
+                this.guessOne.innerText = input.substring(0, 1)
+            } if (word.substring(1, 2) !== input.substring(1, 2)) {
+                this.guessTwo.innerText = input.substring(1, 2)
+            } if (word.substring(2, 3) !== input.substring(2, 3)) {
+                this.guessThree.innerText = input.substring(2, 3)
+            } if (word.substring(3, 4) !== input.substring(3, 4)) {
+                this.guessFour.innerText = input.substring(3, 4)
+            } if (word.substring(4, 5) !== input.substring(4, 5)) {
+                this.guessFive.innerText = input.substring(4, 5)
+            }
+        }  if (promptDisplay === promptOutput.prompt3) {
             let word = words[2]
-            
-        } else if (promptDisplay === promptOutput.prompt4) {
+            if (word.substring(0, 1) === input.substring(0, 1) || word.substring(1, 2) === input.substring(1, 2) || word.substring(2, 3) === input.substring(2, 3) || word.substring(3, 4) === input.substring(3, 4) || word.substring(4, 5) === input.substring(4, 5)) {
+
+            }  if (word.substring(0, 1) !== input.substring(0, 1)) {
+                this.guessOne.innerText = input.substring(0, 1)
+            }  if (word.substring(1, 2) !== input.substring(1, 2)) {
+                this.guessTwo.innerText = input.substring(1, 2)
+            }  if (word.substring(2, 3) !== input.substring(2, 3)) {
+                this.guessThree.innerText = input.substring(2, 3)
+            }  if (word.substring(3, 4) !== input.substring(3, 4)) {
+                this.guessFour.innerText = input.substring(3, 4)
+            }  if (word.substring(4, 5) !== input.substring(4, 5)) {
+                this.guessFive.innerText = input.substring(4, 5)
+            }
+        } if (promptDisplay === promptOutput.prompt4) {
             let word = words[3]
-            
-        } else if (promptDisplay ===  promptOutput.prompt5) {
+            if (word.substring(0, 1) === input.substring(0, 1) || word.substring(1, 2) === input.substring(1, 2) || word.substring(2, 3) === input.substring(2, 3) || word.substring(3, 4) === input.substring(3, 4) || word.substring(4, 5) === input.substring(4, 5)) {
+
+            }  if (word.substring(0, 1) !== input.substring(0, 1)) {
+                this.guessOne.innerText = input.substring(0, 1)
+            }  if (word.substring(1, 2) !== input.substring(1, 2)) {
+                this.guessTwo.innerText = input.substring(1, 2)
+            }  if (word.substring(2, 3) !== input.substring(2, 3)) {
+                this.guessThree.innerText = input.substring(2, 3)
+            }  if (word.substring(3, 4) !== input.substring(3, 4)) {
+                this.guessFour.innerText = input.substring(3, 4)
+            }  if (word.substring(4, 5) !== input.substring(4, 5)) {
+                this.guessFive.innerText = input.substring(4, 5)
+            }
+        }  if (promptDisplay ===  promptOutput.prompt5) {
             let word = words[4]
-            
+            if (word.substring(0, 1) === input.substring(0, 1) || word.substring(1, 2) === input.substring(1, 2) || word.substring(2, 3) === input.substring(2, 3) || word.substring(3, 4) === input.substring(3, 4) || word.substring(4, 5) === input.substring(4, 5)) {
+
+            }  if (word.substring(0, 1) !== input.substring(0, 1)) {
+                this.guessOne.innerText = input.substring(0, 1)
+            }  if (word.substring(1, 2) !== input.substring(1, 2)) {
+                this.guessTwo.innerText = input.substring(1, 2)
+            }  if (word.substring(2, 3) !== input.substring(2, 3)) {
+                this.guessThree.innerText = input.substring(2, 3)
+            }  if (word.substring(3, 4) !== input.substring(3, 4)) {
+                this.guessFour.innerText = input.substring(3, 4)
+            }  if (word.substring(4, 5) !== input.substring(4, 5)) {
+                this.guessFive.innerText = input.substring(4, 5)
+            }
         }
     }
 }
@@ -125,17 +178,6 @@ submit.addEventListener('click', event => {
         subGuesses.letterDisplay()
     }
 })
-
-
-
-//notes for tracking incoming thoughts & getting back on track (using substring)
-//For the else if statements that involve an alert for an incorrect answer
-//Call the letter display function within the brackets of those conditions
-//For the letter display function, assign variables to catch the incorrect inputs
-//Then have those incorrect inputs display in the divs within the display object
-//The amount of divs represent the amount of guesses remaining, there are eight
-//When all eight divs are filled with letters, then the game is over
-
 
 
 //Code Storage
